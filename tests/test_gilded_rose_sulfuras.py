9dfdbcd4f17f01e3263aa5c08058548cb81c874c
Aveek <aveeek.d22@gmail.com>
2022-02-09 22:32:00 +0100

    Added tests for Sulfuras

--- tests/test_gilded_rose_sulfuras.py
+++ tests/test_gilded_rose_sulfuras.py
@@ -6,16 +6,48 @@ from app.products import Product
 product = Product()
 
 
-class GildedRoseAppBrieTest(unittest.TestCase):
-    ''' Tests for Aged Brie products '''
+class GildedRoseSulfurasTest(unittest.TestCase):
+    ''' Tests for Sulfuras products '''
 
-    def test_regular_item_before_sell_in(self):
-        items = [product.create(name="Aged Brie", sell_in=2, quality=0)]
+    def test_sulfuras_before_sell_in(self):
+        items = [product.create(name="Sulfuras, Hand of Ragnaros", sell_in=5, quality=80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("Aged Brie", items[0].name)
-        self.assertEqual(1, items[0].sell_in)
-        self.assertEqual(2, items[0].quality)
+        self.assertEqual("Sulfuras, Hand of Ragnaros", items[0].name)
+        self.assertEqual(5, items[0].sell_in)
+        self.assertEqual(80, items[0].quality)
+
+    def test_sulfuras_on_sell_in(self):
+        items = [product.create(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("Sulfuras, Hand of Ragnaros", items[0].name)
+        self.assertEqual(0, items[0].sell_in)
+        self.assertEqual(80, items[0].quality)
+
+    def test_sulfuras_after_sell_in(self):
+        items = [product.create(name="Sulfuras, Hand of Ragnaros", sell_in=-5, quality=80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("Sulfuras, Hand of Ragnaros", items[0].name)
+        self.assertEqual(-5, items[0].sell_in)
+        self.assertEqual(80, items[0].quality)
+
+    def test_sulfuras_max_quality_85(self):
+        items = [product.create(name="Sulfuras, Hand of Ragnaros", sell_in=5, quality=85)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("Sulfuras, Hand of Ragnaros", items[0].name)
+        self.assertEqual(5, items[0].sell_in)
+        self.assertEqual(80, items[0].quality)
+
+    def test_sulfuras_max_quality_65(self):
+        items = [product.create(name="Sulfuras, Hand of Ragnaros", sell_in=5, quality=65)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("Sulfuras, Hand of Ragnaros", items[0].name)
+        self.assertEqual(5, items[0].sell_in)
+        self.assertEqual(80, items[0].quality)
 
 
 if __name__ == '__main__':
